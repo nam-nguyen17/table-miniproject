831d3b12c8bba121c5b83025716d6d31779a4daf
Nam Duc Nguyen <nnguyen4@simpsonmfg.com>
2023-08-11 14:53:29 +0700

    adding pagination & infinite scroll for 2 pages

--- src/features/product/index.tsx
+++ src/features/product/index.tsx
@@ -1,28 +1,59 @@
-import React, { useState, useEffect } from "react";
-import ProductListTable from "./components/ProductListTable";
+import React, { useState, useEffect, useMemo } from "react";
+import Table from "../../components/table/Table";
+import Pagination from "./components/Pagination";
+import styles from "./components/styles.module.scss";
+
+let PageSize = 10;
 
 const ProductTable: React.FC = () => {
-  const [products, setProducts] = useState([]);
+  const [productsData, setProductsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  // const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchProductData();
-  });
+  }, []);
 
   const fetchProductData = () => {
+    // let flag = true;
     fetch("./data/products_data.json")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        setProducts(data);
+        setProductsData(data);
+        // setIsLoading(false);
+        // flag = false;
       });
   };
 
-  const headers = ["product_name", "category", "barcode", "price"];
+  const headers = [
+    "product_id",
+    "product_name",
+    "category",
+    "expiration_date",
+    "quantity",
+    "price",
+  ];
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return productsData.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, productsData]);
+
+  console.log(currentTableData);
 
   return (
     <div>
-      <ProductListTable headers={headers} data={products} />
+      <Table headers={headers} data={currentTableData} />
+      <Pagination
+        className={`${styles.pagination_bar}`}
+        currentPage={currentPage}
+        totalCount={productsData.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
     </div>
   );
 };
--- src/features/user/index.tsx
+++ src/features/user/index.tsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
-import UserListTable from "./components/UserListTable";
+import TableRow from "../../components/table/TableRow";
+import styles from "../../components/table/styles.module.scss";
+import TableHeader from "../../components/table/TableHeader";
 
 const UserTable: React.FC = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<any[]>([]);
+  const [visibleCount, setVisibleCount] = useState<number>(10);
+
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch("./data/users_data.json");
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
 
   useEffect(() => {
     fetchUserData();
   }, []);
-
-  const fetchUserData = () => {
-    fetch("./data/users_data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setUsers(data);
-      });
-  };
 
   const headers = [
     "customer_id",
@@ -26,10 +31,27 @@ const UserTable: React.FC = () => {
     "phone_number",
   ];
 
+  const handleScroll = (e: React.UIEvent<HTMLTableSectionElement, UIEvent>) => {
+    const target = e.target as HTMLTableSectionElement;
+    // console.log(target.scrollTop, target.clientHeight, target.scrollHeight);
+    if (target.scrollTop + target.clientHeight >= target.scrollHeight) {
+      setVisibleCount((prevCount) => prevCount + 10);
+      console.log("bottom");
+    }
+  };
+
   return (
-    <div>
-      <UserListTable headers={headers} data={users} />
-    </div>
+    <table className={styles.table}>
+      <TableHeader headers={headers} />
+      <tbody
+        id="infinite-table"
+        className={styles.table__body}
+        onScroll={handleScroll}>
+        {users.slice(0, visibleCount).map((item, index) => (
+          <TableRow key={index} rowData={item} headers={headers} />
+        ))}
+      </tbody>
+    </table>
   );
 };
 
